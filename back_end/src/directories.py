15615fbbb90b15bbd224cb81c83630d9c31f0f30
Robbie <robbiemuir7@gmail.com>
2026-02-15 16:58:33 +0100

    improve logging and serialization (#41)

--- back_end/src/directories.py
+++ back_end/src/directories.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 src_dir = Path(__file__).parent
-root_dir = src_dir.parent
+backend_root_dir = src_dir.parent
 game_cache_dir = src_dir / "game_cache"
-test_dir = root_dir / "tests"
+test_dir = backend_root_dir / "tests"
