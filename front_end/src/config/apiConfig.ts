99b2306790fc0c3c74010d8a3ba3c464117940ff
Robbie Muir <robbiemuir7@gmail.com>
2026-05-12 11:10:04 +0200

    formatting

--- front_end/src/config/apiConfig.ts
+++ front_end/src/config/apiConfig.ts
@@ -4,4 +4,5 @@
  */
 
 export const BACKEND_HOST = process.env.NEXT_PUBLIC_BACKEND_HOST || "localhost";
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || `http://${BACKEND_HOST}:8000/api`;
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || `http://${BACKEND_HOST}:8000/api`;
